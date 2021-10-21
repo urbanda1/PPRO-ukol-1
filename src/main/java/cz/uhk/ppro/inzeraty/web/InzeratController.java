fc3de74a58bd5e14afa67be2b5df4864aae6bfa8
David Urban <urbanda1@uhk.cz>
2021-10-21 22:39:48 +0200

    FINAL UGLY VERSION

--- src/main/java/cz/uhk/ppro/inzeraty/web/InzeratController.java
+++ src/main/java/cz/uhk/ppro/inzeraty/web/InzeratController.java
@@ -2,22 +2,35 @@ package cz.uhk.ppro.inzeraty.web;
 
 import cz.uhk.ppro.inzeraty.model.Inzerat;
 import cz.uhk.ppro.inzeraty.sluzby.PametoveUlozisteInzeratu;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class InzeratController {
     private PametoveUlozisteInzeratu inzeraty = new PametoveUlozisteInzeratu();
-
+    private String kategorie;
 
     @RequestMapping("/")
     public ModelAndView zobrazitMain() {
-        ModelAndView model = new ModelAndView("main");
-        model.addObject("inzeraty", inzeraty.getInzeraty());
+        if (kategorie == null) {
+            kategorie = "";
+        }
 
+        ModelAndView model = new ModelAndView("main");
+
+        List<Inzerat> vysledky;
+
+        vysledky = inzeraty.getInzeratyByKategorie(kategorie);
+
+        if ((kategorie.equals("Nákup")) || (kategorie.equals("Prodej")) || (kategorie.equals("Výměna"))) {
+            model.addObject("inzeraty", vysledky);
+        } else {
+            model.addObject("inzeraty", inzeraty.getInzeraty());
+        }
         return model;
     }
 
@@ -30,11 +43,50 @@ public class InzeratController {
         return model;
     }
 
+    @RequestMapping("/edit-form")
+    public ModelAndView zobrazitEdit(@RequestParam("id") int id) {
+        ModelAndView model = new ModelAndView("edit");
+        Inzerat inzeratEdit = inzeraty.getById(id);
+
+        model.addObject("inzeratEdit", inzeratEdit);
+
+        return model;
+    }
+
+    @PostMapping("/editInzerat")
+    public String editInzerat(@ModelAttribute("inzeratEdit") Inzerat i) {
+
+        inzeraty.getById(i.getId()).setText(i.getText());
+        inzeraty.getById(i.getId()).setKategorie(i.getKategorie());
+        inzeraty.getById(i.getId()).setCena(i.getCena());
+
+        return "redirect:/";
+    }
+
     @PostMapping("/saveInzerat")
     public String create(@ModelAttribute("inzerat") Inzerat i) {
+
         inzeraty.pridej(i);
 
         return "redirect:/";
     }
 
+    @PostMapping("/filterInzeratyByKategorie")
+    public String zobrazFiltr(@RequestParam("kategorie") String k) {
+        kategorie = k;
+
+        return "redirect:/";
+    }
+
+    @RequestMapping("/deleteInzerat")
+    public String deleteInzerat(@RequestParam("heslo") String heslo) {
+
+        for (Inzerat inzerat : inzeraty.getInzeraty()) {
+            if (inzerat.getHesloProUpravu().equals(heslo)) {
+                inzeraty.odstran(inzerat);
+                return "redirect:/";
+            }
+        }
+        return "redirect:/";
+    }
 }
